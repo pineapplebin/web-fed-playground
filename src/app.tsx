8dc034e10163dba8eb5c0c6853e2a3ce0abd1b08
bensoncen <bensoncen@meijiabang.cn>
2018-12-10 13:41:20 +0800

    update: learning useEventCallback

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,31 @@
 import * as React from 'react'
-import { useState } from 'react'
-import { fromEvent } from 'rxjs'
-import EventEmitter from './lib/event-emitter'
+// import { useState } from 'react'
+// import { fromEvent } from 'rxjs'
+// import EventEmitter from './lib/event-emitter'
+import { useEventCallback } from './lib/use-event-callback'
+import { mapTo } from "rxjs/operators";
+import { Observable } from "rxjs";
 
 export default function App (props: { base?: number }) {
-  const [count, setCount] = useState(props.base || 1)
-
-  const EE = new EventEmitter()
-  fromEvent(EE, 'click').subscribe(() => setCount(count + 1))
-
-  return (<div>
-    <h2>counting: {count}</h2>
-    <button onClick={(e) => EE.emit('click', e)}>click</button>
-  </div>)
+  // const [count, setCount] = useState(props.base || 1)
+  //
+  // const EE = new EventEmitter()
+  // fromEvent(EE, 'click').subscribe(() => setCount(count + 1))
+  //
+  // return (<div>
+  //   <h2>counting: {count}</h2>
+  //   <button onClick={(e) => EE.emit('click', e)}>click</button>
+  // </div>)
+  const [clickCallback, value] = useEventCallback(
+    (eventSource$: Observable<React.SyntheticEvent<HTMLButtonElement>>) => {
+      return eventSource$.pipe(
+        mapTo(1000)
+      )
+    })
+  return (
+    <>
+      <h2>counting: {value}</h2>
+      <button onClick={clickCallback}>click me</button>
+    </>
+  )
 }
