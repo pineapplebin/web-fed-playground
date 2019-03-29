b78e05a6d216f2b91f60713f207bbe83c1b902ef
bensoncen <bensoncen@meijiabang.cn>
2019-03-29 17:50:41 +0800

    refactor: refactor the projects structure

---
+++ src/README.md
@@ -0,0 +1,9 @@
+# src
+
+每个目录对应独立的页面，所以`src`内的子目录应该遵循以下结构：
+
+## src 目录
+
+- demo
+  - index.html
+  - main.ts (jsx or tsx or js)
--- webpack.config.js
+++ webpack.config.js
@@ -3,31 +3,34 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 
 const ROOT_PATH = path.resolve(__dirname)
-const APP_PATH = path.resolve(ROOT_PATH, 'src', 'main.tsx')
 const DIST_PATH = path.resolve(ROOT_PATH, 'dist')
+// const APP_PATH = path.resolve(ROOT_PATH, 'src', 'main.tsx')
 
 module.exports = {
-  mode: "development",
-  entry: ['webpack-hot-middleware/client?noInfo=true&reload=true', APP_PATH],
+  mode: 'development',
+  // entry: ['webpack-hot-middleware/client?noInfo=true&reload=true', APP_PATH],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
   output: {
     path: DIST_PATH,
-    filename: "bundle.js"
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js'
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: 'src/index.html' }),
-    new webpack.HotModuleReplacementPlugin(),
+    // new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new webpack.HotModuleReplacementPlugin()
   ],
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: [{
-          loader: "babel-loader"
-        }]
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
       },
       {
         test: /\.tsx?$/,
